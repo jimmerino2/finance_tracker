3fef10ed62436804837266251ffec823bf151762
Jim <141894353+jimmerino2@users.noreply.github.com>
2025-03-03 11:17:30 +0800

    add(api) - Standard sidebar

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+import AppSidebar from "@/composables/AppSidebar.vue";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+      <NuxtPage />
+    </main>
+  </SidebarProvider>
 </template>
